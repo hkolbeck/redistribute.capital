3b475880dc2b9591effcf4e31e4c2a5560ab0040
Hannah Kolbeck <hskolbeck@gmail.com>
2021-01-28 12:22:02 -0800

    Move to async fetch and real csv parses

--- fetch_funds.js
+++ fetch_funds.js
@@ -1,22 +1,27 @@
 let funds = []
 
-let fetch_funds = function() {
-    let xhr = new XMLHttpRequest();
-    xhr.onload = function() {
-        xhr.response.split('\n')
-            .filter(value => value.toString().length > 1)
-            .forEach(line => funds.push({html: line.substring(1, line.length - 1).replaceAll('""', '"')}))
-
-        for (let i = funds.length - 1; i >= 0; i--) {
-            let new_idx = Math.floor(Math.random() * (i + 1));
-            let previous = funds[new_idx];
-            funds[new_idx] = funds[i];
-            funds[i] = previous;
-        }
-    };
-
-    xhr.open("GET", "https://docs.google.com/spreadsheets/d/e/2PACX-1vTS4kW1Z4_14OBGebIDZLXeXWBpuHuAhtzhECJ1L_7FqXgO64uQgiukG6VfWqlpxs-CKMA5-8tyOH7K/pub?gid=0&single=true&output=csv");
-    xhr.send();
+Papa.parsePromise = function(url) {
+    return new Promise(function(complete, error) {
+        Papa.parse(url, {
+            download: true,
+            header: false,
+            complete: complete,
+            error: error
+        });
+    });
 };
 
-fetch_funds()
+
+let fetch_funds = async function () {
+    await Papa.parsePromise("https://docs.google.com/spreadsheets/d/e/2PACX-1vTS4kW1Z4_14OBGebIDZLXeXWBpuHuAhtzhECJ1L_7FqXgO64uQgiukG6VfWqlpxs-CKMA5-8tyOH7K/pub?gid=0&single=true&output=csv")
+        .then(result => result.data.forEach(line => funds.push({html: line})))
+};
+
+let shuffle_funds = function () {
+    for (let i = funds.length - 1; i >= 0; i--) {
+        let new_idx = Math.floor(Math.random() * (i + 1));
+        let previous = funds[new_idx];
+        funds[new_idx] = funds[i];
+        funds[i] = previous;
+    }
+}
