6db1d9a2e115769c8a2c9b0a2864a63834338a21
Cory <cory.kolbeck@streem.pro>
2021-01-31 13:24:39 -0800

    Move to new backend, add faq, report, and add pages

--- funds.js
+++ funds.js
@@ -30,7 +30,6 @@ Papa.parsePromise = function(url) {
 async function fetch_funds() {
     await Papa.parsePromise("https://docs.google.com/spreadsheets/d/e/2PACX-1vSb-RukXOxGXgb29ULm7RwI_927JsNIwcBcWwHTSrNV1xKa_N81PXzgRUnyIcxF8Kg1r2JPJcw2FC1_/pub?gid=0&single=true&output=csv")
         .then(result => result.data.forEach(line => {
-            console.log(JSON.stringify(line))
             funds.push({html: line[0], type: line[1], sha: line[2]})
         }))
 }
@@ -50,13 +49,13 @@ function find_payment_links(tweet) {
     let venmo = tweet.match(/[Vv][Ee][Nn][Mm][Oo]:?\s*@?\s*([\w-]+)/)
     if (venmo) {
         $(".payment_links")
-            .append(`<a href="https://venmo.com/?txn=pay&audience=private&recipients=${venmo[1]}" target="_blank" rel="noopener noreferrer"><img alt="Venmo" height="64" width="64" src="https://cdn1.venmo.com/marketing/images/branding/venmo-icon.svg"/></a>`)
+            .append(`&nbsp;<a href="https://venmo.com/?txn=pay&audience=private&recipients=${venmo[1]}" target="_blank" rel="noopener noreferrer"><img alt="Venmo" height="64" width="64" src="https://cdn1.venmo.com/marketing/images/branding/venmo-icon.svg"/></a>&nbsp;`)
     }
 
     let cashapp = tweet.match(/(\$[a-zA-Z][\w-]+)|[Cc][Aa][Ss][Hh]-?[Aa][Pp][Pp]:?\s*([a-zA-Z][\w-]+)/)
     if (cashapp) {
         $(".payment_links")
-            .append(`<a href="https://cash.me/${cashapp[2] || cashapp[1]}" target="_blank" rel="noopener noreferrer"><img alt="CashApp" height="64" width="64" src="https://cash.app/icon-196.png"/></a>`)
+            .append(`&nbsp;<a href="https://cash.me/${cashapp[2] || cashapp[1]}" target="_blank" rel="noopener noreferrer"><img alt="CashApp" height="64" width="64" src="https://cash.app/icon-196.png"/></a>&nbsp;`)
     }
 
     if (cashapp || venmo) {
@@ -65,7 +64,7 @@ function find_payment_links(tweet) {
 }
 
 function load_tweet(tweet) {
-    $(".fund_box").css("visibility", "hidden").css("background-color", "#FEFEFE").html(tweet)
+    $(".fund_box").css("visibility", "hidden").css("background-color", "#FEFEFE").removeAttr("height").html(tweet)
     $(".payment_links").empty().css("visibility", "hidden")
     $(".loading").css("visibility", "visible")
 
@@ -91,16 +90,76 @@ function load_tweet(tweet) {
     }, 500)
 }
 
-function render_fund(fund) {
-    //
+function load_gofundme(gofundme) {
+    $(".fund_box").css("visibility", "hidden").css("background-color", "#FEFEFE").html(gofundme)
+    $(".payment_links").empty().css("visibility", "hidden")
+    $(".loading").css("visibility", "visible")
 
-    if (fund.html) {
+    let checkLoaded = null
+
+    let checkTimeout = setTimeout(() => {
+        clearInterval(checkLoaded)
+        $(".loading").css("visibility", "hidden")
+        $(".fund_box").html("<b>Couldn't load fund 😩<br>Please try again!</b>").css("visibility", "visible")
+        console.log("Couldn't load gofundme!")
+    }, 5000)
+
+    checkLoaded = setInterval(() => {
+        let rendered = $(".fund_box").find(".gfm-embed-iframe");
+        console.log("Trying to load gofundme!")
+        if (rendered) {
+            clearTimeout(checkTimeout)
+            clearInterval(checkLoaded)
+            $(".loading").css("visibility", "hidden")
+            setTimeout(() => {
+                $(".fund_box").css("visibility", "visible").attr("height", "500px")
+                $(".gfm-embed").attr("height", "450px")
+                console.log("Loaded gofundme!")
+            }, 500)
+        }
+    }, 500)
+}
+
+function load_facebook(post) {
+    $(".fund_box").css("visibility", "hidden").css("background-color", "#FEFEFE").removeAttr("height").html(post)
+    $(".payment_links").empty().css("visibility", "hidden")
+    $(".loading").css("visibility", "visible")
+
+    let checkLoaded = null
+
+    let checkTimeout = setTimeout(() => {
+        clearInterval(checkLoaded)
+        $(".loading").css("visibility", "hidden")
+        $(".fund_box").html("<b>Couldn't load fund 😩<br>Please try again!</b>").css("visibility", "visible")
+    }, 5000)
+
+    checkLoaded = setInterval(() => {
+        let rendered = $(".fund_box").find("iframe").find("html");
+        if (rendered) {
+            clearTimeout(checkTimeout)
+            clearInterval(checkLoaded)
+            $(".loading").css("visibility", "hidden")
+            setTimeout(() => {
+                $(".fund_box").css("visibility", "visible")
+            }, 500)
+        }
+    }, 500)
+}
+
+function render_fund(fund) {
+    console.log(JSON.stringify(fund))
+    if (fund.type === 'tweet') {
         load_tweet(fund.html)
+    } else if (fund.type === 'gofundme') {
+        load_gofundme(fund.html)
+    } else if (fund.type === 'facebook') {
+        load_facebook(fund.html)
     } else {
-        $(".fund_box").text(`Unable to fetch fund data for ${fund}`)
+        $(".fund_box").text(`Something went wrong. Please report this fund as broken by clicking the 💔 button in
+        the lower left corner.`)
     }
 
-    $(".report_bt").attr("href", `https://docs.google.com/forms/d/e/1FAIpQLSc_kpPJmD5VVRlh1OXjise7EiBEhJKVEaLJqYZLCI_nirHi2Q/viewform?usp=pp_url&entry.1420691641=${fund.sha}`)
+    $(".report_bt").attr("href", `./report.html?fid=${fund.sha}`)
     $(".fund_box_wrapper").css("visibility", "visible")
 }
 
